ea4ef5d1ab69817b2c5342d8fa449680c6249ad9
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-09-14 19:28:20 -0300

    refactor: updated bonus

--- src/main_bonus.c
+++ src/main_bonus.c
@@ -6,37 +6,33 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 12:30:16 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/09/07 20:20:05 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/09/14 19:25:12 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include "libft.h"
 #include <fcntl.h>
 
 int	main(int argc, char *argv[])
 {
-	t_stack		stack_a;
-	t_stack		stack_b;
-	char		*str;
-	int			is_sorted;
+	t_sort	sort;
+	char	*str;
+	int		is_sorted;
 
 	if (argc == 1)
 		return (0);
 	if (check_args(argc, argv))
 		exit_program(1, "Error", STDERR);
-	create_stack(&stack_a, argc, argv);
-	stack_b.top = NULL;
-	str = get_next_line(0);
+	init_sort(&sort, argc, argv);
+	str = get_next_line(STDIN);
 	while (str)
 	{
 		ft_strupd(&str, ft_substr(str, 0, ft_strlen(str) - 1));
-		psl(&stack_a, &stack_b, str);
-		ft_strupd(&str, get_next_line(0));
+		psl(&sort.stack_a, &sort.stack_b, str);
+		ft_strupd(&str, get_next_line(STDIN));
 	}
-	is_sorted = stack_not_sorted(&stack_a, &stack_b);
-	free_stack(&stack_a);
-	free_stack(&stack_b);
+	is_sorted = stack_not_sorted(&sort.stack_a, &sort.stack_b);
+	free_sort(&sort);
 	if (is_sorted == 0)
 		exit_program(0, "OK", STDOUT);
 	else
